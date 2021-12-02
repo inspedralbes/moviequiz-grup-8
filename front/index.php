26989d65ef24a32e43bfd7b285463727b25bc34f
a18polpuipui <a18polpuipui@inspedralbes.cat>
2021-12-02 10:16:54 +0100

    mejoras buscador

--- front/index.php
+++ front/index.php
@@ -13,6 +13,7 @@
 
 <body>
 <form action="buscador.php" method="POST">
+<input type="text" id="keywords" name="keywords" size="30" maxlength="30" >
 <input type="submit" name="search" id="search" value="Buscar">
 </form>
 </body>
