7e77f1675f8af1093a48d4cca14761fe52e9e8e5
a20alvlozhue <a20alvlozhue@inspedralbes.cat>
2021-12-02 09:52:42 +0100

    header v1

---
+++ front/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+
+<body>
+    <div class="navbar-fixed">
+
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#!" class="brand-logo"><img src="img/logo.png" width="200px"></a>
+                <a class="waves-effect waves-light btn right hide-on-med-and-down">Login</a>
+            </div>
+        </nav>
+
+    </div>
+</body>
+
+</html>
