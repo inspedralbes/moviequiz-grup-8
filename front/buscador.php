e65cffdae6e02f6f839a892f1c927dd777ebec11
a18polpuipui <a18polpuipui@inspedralbes.cat>
2021-12-03 10:22:38 +0100

    buscar pelis

--- front/buscador.php
+++ front/buscador.php
@@ -21,8 +21,8 @@
         <div class="nav-wrapper">
             <form>
                 <div class="input-field">
-                    <input id="nombrepeli" type="search" required>
-                    <label class="label-icon" for="search" id="buscar"><i class="material-icons">search</i></label>
+                    <input id="nombrepeli" type="buscar" required>
+                    <label class="label-icon" for="nombrepeli" id="buscar"><i class="material-icons">search</i></label>
                     <i class="material-icons">close</i>
                 </div>
             </form>
