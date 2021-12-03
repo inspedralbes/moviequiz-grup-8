c0f8282b90574a075732d3d5965ee54006d32f74
a18polpuipui <a18polpuipui@inspedralbes.cat>
2021-12-03 10:17:37 +0100

    input

--- front/index.php
+++ front/index.php
@@ -20,7 +20,7 @@
     
 <form action="buscador.php" method="POST">
 <input type="submit" name="search" id="search" value="Buscar">
-
+<input class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</input>
 </form>
 </form>
 </body>
