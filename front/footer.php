5f94562f276c7544ae09d9a91384ea6ceddeca48
a20alvlozhue <a20alvlozhue@inspedralbes.cat>
2021-12-21 09:22:37 +0100

    index footer css

--- front/footer.php
+++ front/footer.php
@@ -1,42 +1,50 @@
 <div class="footer">
-<footer class="#9575cd deep-purple lighten-2">
-          <div class="container">
+    <footer class="#9575cd deep-purple lighten-2">
+        <div class="container">
             <div class="row">
-              <div class="col l12 s1">
-                <h5 class="white-text">Datos de contacto</h5>
-                <p class="grey-text text-lighten-4">Necesitas ayuda?</p>
-                <i class="material-icons prefix">call </i><p>975 879 495</p>
-                <i class="material-icons prefix">message </i><p>help@moviequiz5.com</p>
-              </div>
-             
-             
+                <div class="col l12 s1">
+                    <h5 class="white-text">Datos de contacto</h5>
+                    <p class="grey-text text-lighten-4">Necesitas ayuda?</p>
+                    <i class="material-icons prefix">call </i>
+                    <p>975 879 495</p>
+                    <i class="material-icons prefix">message </i>
+                    <p>help@moviequiz5.com</p>
+                </div>
+
+
             </div>
             <div class="col l12 s12 right">
                 <h5 class="white-text">Nosotros somos MovieQuiz5</h5>
                 <ul>
-                  <li><i class="material-icons prefix">looks_one</i><a class="grey-text text-lighten-3" href="#!">Sobre Nosotros</a></li><br>
-                  <li><i class="material-icons prefix">looks_two</i><a class="grey-text text-lighten-3" href="#!">Politica de Privacidad</a></li><br>
-                  <li><i class="material-icons prefix">looks_3</i><a class="grey-text text-lighten-3" href="#!">Trabaja con Nosotros</a></li><br>
-                  <li><i class="material-icons prefix">looks_4</i><a class="grey-text text-lighten-3" href="#!">Prensa</a></li><br>
-                  
-                </ul>
-              </div>
+                    <li><i class="material-icons prefix">looks_one</i><a class="grey-text text-lighten-3"
+                            href="#!">Sobre Nosotros</a></li><br>
+                    <li><i class="material-icons prefix">looks_two</i><a class="grey-text text-lighten-3"
+                            href="#!">Politica de Privacidad</a></li><br>
+                    <li><i class="material-icons prefix">looks_3</i><a class="grey-text text-lighten-3"
+                            href="#!">Trabaja con Nosotros</a></li><br>
+                    <li><i class="material-icons prefix">looks_4</i><a class="grey-text text-lighten-3"
+                            href="#!">Prensa</a></li><br>
+                    © 2021 Copyright Text
+                    <a class="grey-text text-lighten-4 right" href="#!">Alvaro Lozano | Pol Puig</a>
 
-              <div class="col l12 s12 right">
+                </ul>
+            </div>
+
+            <div class="col l12 s12 right">
                 <h5 class="white-text">Cookies</h5>
                 <ul>
-                <li><i class="material-icons prefix">looks_one</i><a class="grey-text text-lighten-3" href="#!">Politica de cookies</a></li><br>
-                  <li><i class="material-icons prefix">looks_two</i><a class="grey-text text-lighten-3" href="#!">Configurar cookies</a></li><br>
-                  
-                </ul>
-              </div>
+                    <li><i class="material-icons prefix">looks_one</i><a class="grey-text text-lighten-3"
+                            href="#!">Politica de cookies</a></li><br>
+                    <li><i class="material-icons prefix">looks_two</i><a class="grey-text text-lighten-3"
+                            href="#!">Configurar cookies</a></li><br>
 
-            © 2021 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">Alvaro Lozano | Pol Puig</a>
-          
-          </div>
-          
-          
-           
-        </footer>
+                </ul>
+            </div>
+
+
+        </div>
+
+
+
+    </footer>
 </div>
--- front/index.php
+++ front/index.php
@@ -33,41 +33,47 @@
                 <div class="masvistas">
                     <h4>Peliculas Recomendadas</h4>
 
-                <div class="carousel center-align">
-                        <a class="carousel-item" href="#one!"><img src="https://api.lorem.space/image/movie?w=159&h=220"></a>
-                        <a class="carousel-item" href="#two!"><img src="https://api.lorem.space/image/movie?w=158&h=220"></a>
-                        <a class="carousel-item" href="#three!"><img src="https://api.lorem.space/image/movie?w=153&h=220"></a>
-                        <a class="carousel-item" href="#four!"><img src="https://api.lorem.space/image/movie?w=156&h=220"></a>
-                        <a class="carousel-item" href="#five!"><img src="https://api.lorem.space/image/movie?w=154&h=220"></a>
-                </div>
+                    <div class="carousel center-align">
+                        <a class="carousel-item" href="#one!"><img
+                                src="https://api.lorem.space/image/movie?w=159&h=220"></a>
+                        <a class="carousel-item" href="#two!"><img
+                                src="https://api.lorem.space/image/movie?w=158&h=220"></a>
+                        <a class="carousel-item" href="#three!"><img
+                                src="https://api.lorem.space/image/movie?w=153&h=220"></a>
+                        <a class="carousel-item" href="#four!"><img
+                                src="https://api.lorem.space/image/movie?w=156&h=220"></a>
+                        <a class="carousel-item" href="#five!"><img
+                                src="https://api.lorem.space/image/movie?w=154&h=220"></a>
+                    </div>
 
                 </div>
 
                 <div class="fav">
                     <h2>Carrito de peliculas</h2>
-                   <br><br><br>
-                   <h3>Puntuacions</h3>
-                   <p>Usuari1-20pts</p>
+                    <br><br><br>
+                    <h3>Puntuacions</h3>
+                    <p>Usuari1-20pts</p>
                 </div>
                 <a class="btn waves-effect waves-light modal-trigger" href="#jugar">JUGAR</a>
+                
             </div>
-            
+            </br>
             <div id="jugar" class="modal">
                 <div class="modal-content">
                     <div id="juego">
-                    <script>
-    fetch('./output_generar_partida.json')
-      .then(response => response.json() )
-      .then(data => {
-        console.log(data);
-        const pelis=data.peliculas;
-        
-        let htmlStr="";
+                        <script>
+                        fetch('./output_generar_partida.json')
+                            .then(response => response.json())
+                            .then(data => {
+                                console.log(data);
+                                const pelis = data.peliculas;
 
-        for (let index = 0; index < pelis.length; index++) {
-          const element = pelis[index];        
+                                let htmlStr = "";
 
-          htmlStr += `<div class='card'>
+                                for (let index = 0; index < pelis.length; index++) {
+                                    const element = pelis[index];
+
+                                    htmlStr += `<div class='card'>
                     <h2>${element.Nombre}</h2>
                     <img src='${element.Poster}'>
                     <div>
@@ -89,33 +95,33 @@
                       </label>
                     </div>
                     </div>`;
-        }
-        htmlStr += `<button id='enviar'>JUGAR</button>`;
-        document.getElementById("juego").innerHTML=htmlStr;
+                                }
+                                htmlStr += `<button id='enviar'>JUGAR</button>`;
+                                document.getElementById("juego").innerHTML = htmlStr;
 
-        });
-    
-</script>
-                           
-                            <div>
-                                <a id="guardar" class="waves-effect waves-light btn #1e88e5 blue darken-1" name="guardar">Guardar Partida</a>
-                            </div>
-                           
-                         
+                            });
+                        </script>
 
+                        <div>
+                            <a id="guardar" class="waves-effect waves-light btn #1e88e5 blue darken-1"
+                                name="guardar">Guardar Partida</a>
+                        </div>
 
-                        </form>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <a href="#" class="waves-effect waves-light btn-flat modal-close">Aceptar</a>
-                </div>
-            </div>
 
 
 
         </form>
-        
+    </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#" class="waves-effect waves-light btn-flat modal-close">Aceptar</a>
+    </div>
+    </div>
+
+
+
+    </form>
+
     </div>
     <script>
     document.addEventListener('DOMContentLoaded', () => {
@@ -123,17 +129,16 @@
         M.Carousel.init(elements, {
             duration: 150,
             dist: -15,
-            
+
         });
-        
+
     });
     </script>
     <script>
     document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-  });
-
+        var elems = document.querySelectorAll('.modal');
+        var instances = M.Modal.init(elems);
+    });
     </script>
     <?php
         require_once('footer.php');
