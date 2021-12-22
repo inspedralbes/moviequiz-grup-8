658ec5fb5b7a8a75f82e5eda859de8b944e6b2bb
a20alvlozhue <a20alvlozhue@inspedralbes.cat>
2021-12-22 08:52:00 +0100

    delete prubea.php

--- front/prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-$favorito = $_POST['favoritoPhp'];
-$valoracion = $_POST['valoracionPhp'];
-$comentario = $_POST['comentarioPhp'];
-
-echo $favorito;
-echo $valoracion;
-echo $comentario;
-?>
