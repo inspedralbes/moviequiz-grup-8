005ccd0860ac04ba66eaed1f861b5c4db7ba4609
a20mohwafche <a20mohwafche@inspedralbes.cat>
2022-01-24 10:02:39 +0100

    1

--- front/logout.php
+++
@@ -1,9 +0,0 @@
-<!-- Destruimos sesion para hacer el LOGOUT -->
-<?php
-    session_start();
-
-    unset($_SESSION['logeado']);
-    session_destroy();
-    header('Location: index.php');
-    exit();
-?>
